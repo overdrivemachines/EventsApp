3106bd6d2dd1e275394961f54231ba8489774a9f
Dipen Chauhan <get.dipen@gmail.com>
2014-12-11 15:04:58 -0800

    Schema in comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,3 +2,23 @@ class Event < ActiveRecord::Base
 	belongs_to :user
 	has_many :photos
 end
+
+=begin
+
+t.string   "title"
+t.date     "fromDate"
+t.time     "fromTime"
+t.time     "untilTime"
+t.date     "untilDate"
+t.string   "address1"
+t.string   "address2"
+t.string   "zip"
+t.string   "description"
+t.string   "url"
+t.string   "email"
+t.string   "phone"
+t.integer  "user_id"
+t.datetime "created_at"
+t.datetime "updated_at"
+
+=end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,12 @@
 class Photo < ActiveRecord::Base
   belongs_to :event
 end
+
+=begin
+
+t.string   "url"
+t.integer  "event_id"
+t.datetime "created_at"
+t.datetime "updated_at"
+	
+=end
