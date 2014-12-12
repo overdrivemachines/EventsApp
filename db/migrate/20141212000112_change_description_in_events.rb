80ea97b45ce3087d8053adedd3705ae9c11305d8
Dipen Chauhan <get.dipen@gmail.com>
2014-12-11 16:15:31 -0800

    Removed limit

--- db/migrate/20141212000112_change_description_in_events.rb
+++ db/migrate/20141212000112_change_description_in_events.rb
@@ -1,5 +1,5 @@
 class ChangeDescriptionInEvents < ActiveRecord::Migration
   def change
-  	change_column(:events, :description, :text)
+  	change_column(:events, :description, :text, :limit => nil)
   end
 end
