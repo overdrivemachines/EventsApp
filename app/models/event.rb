5f99b1fb8a51c8b0c828ef442d7bb4e5dc51f036
Dipen Chauhan <get.dipen@gmail.com>
2014-12-11 16:05:45 -0800

    Added validations. event_dates_make_sense incomplete

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,30 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
 	has_many :photos
+
+	validates :title, presence: true,
+						length: { in: 15..110 }
+	validates :fromDate, presence: true
+	validates :fromTime, presence: true
+	validates :untilTime, presence: true
+	validates :untilDate, presence: true
+	validates :address1, presence: true
+	validates :zip, presence: true
+	validates :description, presence: true
+	validates :phone, presence: true
+
+	# TODO: validates :user_id
+
+	validate :event_dates_make_sense
+
+	private
+	def event_dates_make_sense
+		today = Date.today
+		difference = fromDate - today
+		if difference.to_i < 0
+			errors.add(:fromDate, 'has to be today or in the future.')
+		end
+	end
 end
 
 =begin
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118233842) do
+ActiveRecord::Schema.define(version: 20141212000112) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20141118233842) do
     t.string   "address1"
     t.string   "address2"
     t.string   "zip"
-    t.string   "description"
+    t.text     "description", limit: 255
     t.string   "url"
     t.string   "email"
     t.string   "phone"
