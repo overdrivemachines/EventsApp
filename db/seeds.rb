48e8b4c8ae87f705076d67af1823aba1eb6e9312
Dipen Chauhan <get.dipen@gmail.com>
2014-12-11 17:13:35 -0800

    Sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.new(
+	email: 'admin@example.com',
+	password: 'administrator',
+	password_confirmation: 'administrator'
+)
+admin.save!
+
+me = User.new(
+	email: 'get.dipen@gmail.com',
+	password: 'happychico',
+	password_confirmation: 'happychico'
+)
+me.save!
+
+# Event.create([
+# 	{
+# 		title: 'Former art faculty exhibit',
+# 		fromDate: ,
+# 		fromTime: ,
+# 		untilTime: ,
+# 		untilDate: ,
+# 		address1: ,
+# 		address2: ,
+# 		zip: ,
+# 		description: ,
+# 		url: ,
+# 		email: ,
+# 		phone: 
+# 	}
+# ])
